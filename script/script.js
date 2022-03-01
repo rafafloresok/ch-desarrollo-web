567695e5a52f1c0b9633e03b9e62b8a1c171bcdc
rafafloresok <rafafloresok@gmail.com>
2022-03-01 17:47:52 -0300

    seccion quienes somos se elimina bootstrap

--- script/script.js
+++ script/script.js
@@ -4,3 +4,12 @@ const menuCover = document.querySelector(".menu-cover");
 navToggle.addEventListener("click", () => {
   menuCover.classList.toggle("display-none");
 });
+
+const cardButtonArticle = document.querySelectorAll(".card__button--article");
+const article = document.querySelector(".articulo");
+const frame = document.querySelector(".articulo__frame");
+
+cardButtonArticle.forEach(el => el.addEventListener("click", () => {
+  frame.src = el.id;
+  article.classList.remove("display-none");
+}));
